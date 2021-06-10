d6b7371b2c5764e2f38f1ef2c3355f7694194aad
MANNAN <bsse0920@iit.du.ac.bd>
2021-06-10 12:01:45 +0600

    api routes added

--- infocom-backend/routes/api.php
+++ infocom-backend/routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('helptopics', [\App\Http\Controllers\Api\HelpTopicController::class, 'index']);
 
 Route::middleware(['auth:api'])->group(function(){
-    //helptopics
+    //help topics
     Route::post('helptopics', [\App\Http\Controllers\Api\HelpTopicController::class, 'create']);
 
     //departments
@@ -26,6 +26,24 @@ Route::middleware(['auth:api'])->group(function(){
     Route::post('departments', [\App\Http\Controllers\Api\DepartmentController::class, 'create']);
 
     //slaplans
-    Route::get('slaplans', []);
-    Route::post('slaplans', []);
+    Route::get('slaplans', [\App\Http\Controllers\Api\SlaPlanController::class, 'index']);
+    Route::post('slaplans', [\App\Http\Controllers\Api\SlaPlanController::class, 'create']);
+
+    //users
+    Route::post('login', [\App\Http\Controllers\Api\UserController::class, 'login']);
+
+    //callcenteragents
+    Route::post('callcenteragents', [\App\Http\Controllers\Api\CallcenterAgentController::class, 'create']);
+
+    //supportagents
+    Route::post('supportagents', [\App\Http\Controllers\Api\SupportAgentController::class, 'create']);
+
+    //customers
+    Route::post('customers', [\App\Http\Controllers\Api\CustomerController::class, 'create']);
+
+    //complains
+    Route::get('complains', [\App\Http\Controllers\Api\ComplainController::class, 'index']);
+    Route::post('complains', [\App\Http\Controllers\Api\ComplainController::class, 'create']);
+    Route::put('complains/{complain_id}', [\App\Http\Controllers\Api\ComplainController::class, 'update']);
+    Route::delete('complains/{complain_id}', [\App\Http\Controllers\Api\ComplainController::class, 'destroy']);
 });
