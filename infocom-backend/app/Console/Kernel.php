eea310ed33c4013f29e1dc840ce529cc153ac44d
fuadmmnf <bsse0920@iit.du.ac.bd>
2021-07-01 00:27:38 +0600

    fixed assigned time save code

--- infocom-backend/app/Console/Kernel.php
+++ infocom-backend/app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(new CheckComplainTimeLimitExceed)->everyFiveMinutes();
+        $schedule->call(new CheckComplainTimeLimitExceed)->everyMinute();
     }
 
     /**
