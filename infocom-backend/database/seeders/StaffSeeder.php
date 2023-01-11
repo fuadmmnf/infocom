0abd0d36c337dd7b5a713dac667f0c377e68d220
fuad <fuad@fuads-MacBook-Pro.local>
2023-01-11 18:43:46 +0600

    wrote staff seeder

--- infocom-backend/database/seeders/StaffSeeder.php
+++ infocom-backend/database/seeders/StaffSeeder.php
@@ -2,12 +2,43 @@
 
 namespace Database\Seeders;
 
+use App\Models\CallcenterAgent;
+use App\Models\Department;
+use App\Models\SupportAgent;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class StaffSeeder extends Seeder
 {
     public function run()
     {
+        $callcenterUsers = User::factory()->count(5)->create();
+        $departments = Department::all();
+        foreach ($callcenterUsers as $user) {
+            $user->assignRole('callcenter_agent');
+            $callcenter = new CallcenterAgent();
+            $callcenter->user_id = $user->id;
+            $callcenter->save();
+        }
 
+        $supportUsers = User::factory()->count($departments->count() * 2)->create();
+        $departments = Department::all();
+        foreach (range(0, $departments->count()) as $i) {
+            $dept = $departments->slice($i, 1);
+            foreach (range(0, 1) as $j) {
+
+                $user->assignRole('support_agent');
+                $supportagent = new SupportAgent();
+                $supportagent->user_id = $supportUsers->slice($i * 2 + $j, 1)->id;
+                $supportagent->department_id = $dept->id;
+                $supportagent->save();
+
+                if ($j == 0) { //assign dept head
+                    $dept->leader_id = $supportagent->id;
+                    $dept->save();
+
+                }
+            }
+        }
     }
 }
