464d1d5e2cfc9be38b60a46649f803ffa3d4c1cf
fuadmmnf <bsse0920@iit.du.ac.bd>
2024-01-13 20:03:33 +0600

    customer pagination working

--- infocom-backend/database/seeders/CustomerSeeder.php
+++ infocom-backend/database/seeders/CustomerSeeder.php
@@ -11,7 +11,7 @@ class CustomerSeeder extends Seeder
 {
     public function run()
     {
-        $users = User::factory()->count(10)->create();
+        $users = User::factory()->count(30)->create();
         $pops = PopAddress::all();
         foreach ($users as $user){
             $user->assignRole('customer');
--- infocom-frontend/src/pages/Dashboard/Customers.vue
+++ infocom-frontend/src/pages/Dashboard/Customers.vue
@@ -10,7 +10,52 @@
       :pagination.sync="pagination"
       @row-click="(e, row, idx) => {openCustomerModal(row)}"
       @update:pagination="({page}) => {fetchCustomers(page)}"
+      @request="onRequest"
     >
+      <template v-slot:pagination="scope">
+        <q-btn
+          v-if="scope.pagesNumber > 2"
+          icon="first_page"
+          color="grey-8"
+          round
+          dense
+          flat
+          :disable="scope.isFirstPage"
+          @click="scope.firstPage"
+        />
+
+        <q-btn
+          icon="chevron_left"
+          color="grey-8"
+          round
+          dense
+          flat
+          :disable="scope.isFirstPage"
+          @click="scope.prevPage"
+        />
+
+        <q-btn
+          icon="chevron_right"
+          color="grey-8"
+          round
+          dense
+          flat
+          :disable="scope.isLastPage"
+          @click="scope.nextPage"
+        />
+
+        <q-btn
+          v-if="scope.pagesNumber > 2"
+          icon="last_page"
+          color="grey-8"
+          round
+          dense
+          flat
+          :disable="scope.isLastPage"
+          @click="scope.lastPage"
+        />
+      </template>
+
       <template v-slot:top-right>
         <div class="flex flex-center items-center">
           <q-btn label="Create" @click="() => {
@@ -248,10 +293,12 @@ export default {
   },
   data() {
     return {
+      loading: false,
       showCustomerForm: false,
       pagination: {
         page: 1,
-        rowsPerPage: 20
+        rowsPerPage: 3,
+        rowsNumber: 10
       },
       departments: [],
       customers: [],
@@ -272,8 +319,28 @@ export default {
     }
   },
   mounted() {
+    this.onRequest({
+      pagination: this.pagination,
+      filter: undefined
+    })
   },
   methods: {
+    onRequest (props) {
+      const { page, rowsPerPage, sortBy, descending } = props.pagination
+      const filter = props.filter
+
+      this.loading = true
+      this.fetchCustomers(page)
+      this.pagination.page = page
+      this.pagination.sortBy = sortBy
+      this.pagination.descending = descending
+
+      // this.pagination.rowsPerPage = rowsPerPage
+
+
+      // ...and turn of loading indicator
+      this.loading = false
+    },
     openCustomerModal(customer) {
       if (customer === null) {
         this.customerForm = customerFormTemplate()
